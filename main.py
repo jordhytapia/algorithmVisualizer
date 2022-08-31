02d4d38de6b61fbac909a2a8e15339902f8c9f21
jordhy <jordhy93@gmail.com>
2022-08-30 17:29:08 -0700

    create drawInfo class

--- main.py
+++ main.py
@@ -2,3 +2,45 @@ import pygame
 import random
 
 pygame.init()
+
+class DrawInfo:
+    BLACK = 0, 0, 0
+    WHITE = 255, 255, 255
+    GREEN = 0, 255, 0
+    RED = 255, 0, 0
+    GREY = 128, 128, 128
+    BG_COLOR = WHITE
+
+    sidePad = 100
+    topPad = 150
+
+    def __init__(self, width, height, lst):
+        self.width = width
+        self.height = height
+
+        self.window = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Sorting Algorithm Visual")
+        self.set_list(lst)
+
+    def set_list(self, lst):
+        self.lst = lst
+        self.minVal = min(lst)
+        self.maxVal = max(lst)
+
+        self.blockWidth = round((self.width - self.sidePad) / len(lst))
+        self.blockHeight = round((self.height - self.topPad) / (self.maxVal - self.minVal))
+        self.starX = self.sidePad // 2
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
